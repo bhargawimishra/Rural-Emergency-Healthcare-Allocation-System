f2d153855cf55835ba8a055ca452addadf0bc636
Bhargawi Mishra <bhargawimishra07@gmail.com>
2026-04-07 22:32:10 +0530

    Update hospital.h

--- hospital.h
+++ hospital.h
@@ -1,1 +1,27 @@
 
+#include<iosstream>
+#include<string>
+#include<hospital.h>
+using namespace std;
+
+class Hospital
+{
+private:
+    string hospitalid;
+    string name;
+    string location;
+    int availablebeds;
+    int totalbeds;
+    string emergency;
+public:
+    Hospital();
+    string getid();
+    string getname();
+    string getlocation();
+    int availablebeds;
+    bool emergencyavailableornot();
+    void sethospital(string id, string nam, string loc, int avail, int total, bool emer);
+    void display();
+
+};
+#endif // hospital_h
