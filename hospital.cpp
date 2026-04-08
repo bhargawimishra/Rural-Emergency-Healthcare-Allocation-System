7cf7ab5fa2f5308783a5500e1a1851c5f1c0a041
Bhargawi Mishra <bhargawimishra07@gmail.com>
2026-04-08 10:01:40 +0530

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -1,1 +1,47 @@
+#include<iostream>
 
+void Hospital::Hospital()
+{
+
+}
+
+void Hospital::void sethospital(string id, string nam, string loc, int avail, int total, bool emer);
+{
+    hospitalid=id;
+    name=nam;
+    location=loc;
+    availablebeds=vail;
+    totalbeds=total;
+    emergencyavailableornot=emer;
+}
+
+void Hospital::void display();
+{
+
+}
+ string Hospital::string getid()
+ {
+
+ }
+
+  string Hospital::string getname()
+ {
+
+ }
+
+  string Hospital::string getlocation()
+ {
+
+ }
+ int Hospital::availablebeds()
+ {
+
+ }
+  int Hospital::totalbeds()
+ {
+
+ }
+  bool Hospital::emergencyavailableornot()
+ {
+
+ }
